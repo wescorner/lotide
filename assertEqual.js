1cd38cec5ce5d0c889c2097f7e40289ca7d7586d
Wesley Corner <wescorner@gmail.com>
2022-06-27 18:45:13 +0000

    working assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighhouse labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("dog", "cat");
