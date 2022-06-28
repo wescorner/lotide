b11fde616d5057bc3c25681e384adf16f8e0a47e
Wesley Corner <wescorner@gmail.com>
2022-06-28 17:16:34 +0000

    added without.js to return array without given items

---
+++ without.js
@@ -0,0 +1,36 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for(const item of source) {
+    if(!itemsToRemove.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
