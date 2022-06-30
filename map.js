ffca53d1c39f3bd8a78c88ade1d28d185a8df583
Wesley Corner <wescorner@gmail.com>
2022-06-30 22:02:07 +0000

    added map.js to implement the arrap mapping function

---
+++ map.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, (word) => word[0]);
+const results2 = map(words, (word) => word.toUpperCase());
+const results3 = map(words, (word) => word.slice(1));
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(results2, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
+assertArraysEqual(results3, ['round', 'ontrol', 'o', 'ajor', 'om']);
