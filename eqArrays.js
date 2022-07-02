531253fec5c88e7a3e9a4ca865b5e2af572d899e
Wesley Corner <wescorner@gmail.com>
2022-07-02 01:17:36 +0000

    refactored eqArrays.js with recursion to include cases where elements of arrays were nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -11,14 +11,23 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
-      return false;
+    if(Array.isArray(arr1[i]) || Array.isArray(arr2[i])) {
+      if(!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else {
+      if(arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
   }
   return true;
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [[[4]]]])); // => false
