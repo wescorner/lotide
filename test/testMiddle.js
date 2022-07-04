8c778533f29fdb4032285e4a2846aa0172880853
Wesley Corner <wescorner@gmail.com>
2022-07-04 18:44:05 +0000

    refactor testMiddle as well as some renaming

--- test/testMiddle.js
+++ test/testMiddle.js
@@ -1,5 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3]), 2);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("should return 2 when given [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("should return [2, 3] when given [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+})
