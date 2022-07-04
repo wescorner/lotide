5efc91ab8a8617f7c5a2e495b754735380b0a74b
Wesley Corner <wescorner@gmail.com>
2022-07-04 18:33:46 +0000

    refactor head with chai/mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,14 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("should return 1 when given [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return '5' when given ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("should return 'Hello' when given ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
